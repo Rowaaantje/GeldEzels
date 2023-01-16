93d37a163d9aadb1b2814c11692c94a47db0baf7
Rowaaantje <114189377+Rowaaantje@users.noreply.github.com>
2023-01-16 10:08:10 +0100

    Unity project new / mechanics scripts

---
+++ Assets/Scripts/LevelManager.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelManager : MonoBehaviour
+{
+    public void LoadNextScene()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    // Load previous scene
+    public void LoadPreviousScene()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex - 1);
+    }
+    public void backToMenuNav()
+    {
+        SceneManager.LoadScene(0);
+    }
+}
